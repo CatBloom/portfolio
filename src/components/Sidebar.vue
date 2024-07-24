086b1271c7f11063b48b74225b1267138d954329
CatBloom <shomail.cat@gmail.com>
2024-07-25 01:41:35 +0900

    sidebar作成

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <RouterLink to="/">TOP</RouterLink>
+    <RouterLink to="/profile">Profile</RouterLink>
+    <RouterLink to="/work">Work</RouterLink>
+    <RouterLink to="/skilset">Skilset</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  flex-flow: column;
+  gap: 1rem;
+  width: 100%;
+  a {
+    color: rgb(150, 132, 174);
+    text-decoration: none;
+    transition: 0.4s;
+  }
+}
+</style>
