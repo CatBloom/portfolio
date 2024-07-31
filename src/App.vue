96f1a3514a39c1cefb69643141f31a0aae683255
CatBloom <shomail.cat@gmail.com>
2024-08-01 02:49:27 +0900

    レイアウト修正

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,32 @@
 <template>
   <div class="wrapper" :class="{ dark: isDarkMode }">
-    <header class="fixed-header">
-      <Sidebar />
+    <header class="header">
+      <TopView />
     </header>
     <main class="content">
       <RouterView />
     </main>
+    <footer class="footer"></footer>
   </div>
 </template>
 
 <style scoped lang="scss">
 .wrapper {
-  display: grid;
-  grid-template-columns: 6rem 1fr;
-  height: 100vh;
-}
-
-.fixed-header {
-  top: 30%;
-  align-self: flex-start;
-  position: sticky;
-  padding: 1rem 1rem 1rem 2rem;
+  min-height: 100vh;
+  height: 100%;
 }
 
 .content {
   padding: 2rem;
 }
+
+.footer {
+  height: 10vh;
+}
 </style>
 
 <script setup lang="ts">
-import Sidebar from './components/Sidebar.vue';
+import TopView from './views/TopView.vue';
 import { RouterView } from 'vue-router';
 import { useDarkModeStore } from './stores/dark-mode';
 import { storeToRefs } from 'pinia';
---
+++ src/components/PageContent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page-content">
+    <h2 class="title">
+      <slot name="title"></slot>
+    </h2>
+    <h3 class="sub-titile">
+      <slot name="sub-title"></slot>
+    </h3>
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page-content {
+  margin-top: 2rem;
+}
+.title {
+  font-weight: bold;
+  font-size: 2.5rem;
+  text-align: center;
+}
+.sub-titile {
+  font-weight: bold;
+  font-size: 2rem;
+}
+.content {
+  margin-top: 1rem;
+  font-size: 1rem;
+}
+</style>
